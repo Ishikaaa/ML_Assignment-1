3e5c5e54c1dc7630dfcea2cbb96049872fc225df
ishikaaa <gargishika1998>
2018-08-10 18:43:16 +0530

    Assignment-3 completed

---
+++ MLassignment-3.py
@@ -0,0 +1,26 @@
+#http://127.0.0.1:8888/notebooks/ML_Assignment.ipynb
+#Q1.
+def cp(a,b,ab):
+    BA=((AB)*B)/A
+    print(BA)
+A = 11 / 36
+B = 6 / 36
+AB = 2 / 36
+cp(A,B,AB)
+
+#Q2.
+def bt(A,B,RA,RB):
+    AR=((RA)*A)/((A*RA)+(B*RB))
+    print(AR)
+A=1/3;B=1/3
+RA=6/10;RB=3/7
+bt(A,B,RA,RB)
+
+#Q3.
+def BT(T,D,TD):
+    DT=((TD)*D)/T
+    print(DT)
+T=2/3
+D=1/8
+TD=2/3
+BT(T,D,TD)
